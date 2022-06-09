0be22c9ff3f5162254a9c3e17ad8fb7243d16eac
Neelam-zanvar <neelam.zanvar@minutuscomputing.com>
2022-06-09 13:16:34 +0530

    new files added

---
+++ public/scripts/test.sh
@@ -0,0 +1,10 @@
+$ cd public
+$ rm -rf sitemap
+$ mkdir sitemap
+$ cd ..
+$ cd scripts
+$ node ./sitemap-posts.js
+$ node ./compress-map.js
+$ node ./create-one-map.js
+
+$ curl http://google.com/ping?sitemap=https://website.com/sitemap.xml
