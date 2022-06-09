a37d0856a7192a9540d8e8a8b1449ca8b0fb98f4
Neelam-zanvar <neelam.zanvar@minutuscomputing.com>
2022-06-09 14:58:57 +0530

    made changes to .sh file

--- public/scripts/test.sh
+++ public/scripts/test.sh
@@ -1,10 +1,6 @@
-$ cd public
-$ rm -rf sitemap
-$ mkdir sitemap
-$ cd ..
-$ cd scripts
-$ node ./sitemap-posts.js
-$ node ./compress-map.js
-$ node ./create-one-map.js
-
-$ curl http://google.com/ping?sitemap=https://website.com/sitemap.xml
+cd public
+rm -rf sitemap
+mkdir sitemap
+cd ..
+cd scripts
+curl http://google.com/ping?sitemap=https://website.com/sitemap.xml
